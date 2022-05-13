224c81b1b6a6b5186b164558a29a70b5aeb603a5
Saloni <saloni.stha2002@gmail.com>
2022-05-13 23:10:48 +0545

    changes

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/usrlogin',[UserController1::class,"usrlogin"])->name('usrlogin');
+Route::post('/usrloginCheck',[UserController1::class,"usrloginCheck"])->name('usrlogin.store');
